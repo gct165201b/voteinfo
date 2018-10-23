7627e1b8b1df857ee4455860be9b44507665d278
gct165201b <43592029+gct165201b@users.noreply.github.com>
2018-10-23 14:22:55 +0500

    Selected image displayed

--- voteinfo/js/main.js
+++ voteinfo/js/main.js
@@ -4,12 +4,64 @@
 	var tf = document.querySelectorAll('.test');
 
 
+
+
+
 	// console.log(tf);
 // console.log(tf.length, output.length);
 
 	tf.forEach( function(input, index) {
 		// statements
-		input.addEventListener('keyup', function() {
-			output[index].innerHTML = "<h4>" + input.placeholder + "</h4>" + input.value ;
-		});
+		
+			input.addEventListener('keyup', function() {
+				output[index].innerHTML = "<h5>" + input.placeholder + "</h5>" + input.value ;
+			});
+		
 	});
+
+
+
+
+
+// Select and display symbole image.
+
+var symboleImg = document.querySelectorAll('#symbole-img');
+var targets = document.querySelectorAll('.card-img-top');
+
+
+
+
+// symboleImg.forEach( function(input, index) {
+// 		// statements
+		
+// 			input.addEventListener('change', function() {
+// 				var path = input.value.split('\\')[2];
+// 				targets[index].src = 'img/' + path;
+// 				targets[index].style.display = 'block';
+// 			});
+		
+// 	});
+
+
+
+
+symboleImg.forEach(function(imgInput, ind) {
+	imgInput.onchange = function() {
+		var file = imgInput.files[0];
+		var reader = new FileReader();
+
+		// console.log(file);
+		reader.addEventListener('load', function() {
+			// console.log(reader.result);
+			targets[ind].src = reader.result;
+			targets[ind].style.display = 'block';
+		}, false);
+
+
+
+		if(file) {
+			reader.readAsDataURL(file);
+		}
+	};
+});
+
